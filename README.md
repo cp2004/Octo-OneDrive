a0e737d3ba5891211e5cfadef8246ebe5c5efb2a
Charlie Powell <31997505+cp2004@users.noreply.github.com>
2022-06-05 21:47:46 +0100

    :tada: Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# OctoPrint OneDrive Communication Module
+
+This is used in my two plugins - [OctoPrint OneDrive Backup](https://github.com/cp2004/OctoPrint-OneDrive-Backup) and [OctoPrint OneDrive File Sync](https://github.com/cp2004/OctoPrint-OneDriveFileSync).
+
+It is made for my own purposes to unify the code across the two plugins, making maintenance a bit easier. As such it has no documentation or stable API guarantees and I don't recommend you try and use it for your project.
+
+It is hosted on PyPi and can be installed with pip:
+
+```
+pip install octo-onedrive
+```
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import versioneer
+
+NAME = "octo_onedrive"
+VERSION = versioneer.get_version()
+DEPENDENCIES = [
+    "msal>=1.18.0,<2",
+    "cryptography"
+]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author="Charlie Powell",
+    author_email="cp2004.github@gmail.com",
+    description="OctoPrint OneDrive Communication Module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cp2004/Octo-OneDrive",
+    project_urls={
+        "Bug Tracker": "https://github.com/cp2004/Octo-OneDrive/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only"
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    cmdclass=versioneer.get_cmdclass(),
+)
